b785630cd5f6800fbd8958cf92e87c81973d3330
AndreasSasDev <directive.athena@gmail.com>
2024-01-19 18:09:57 +0100

    Update Sprite.cs

--- src/AterraEngine/Draw/Sprite.cs
+++ src/AterraEngine/Draw/Sprite.cs
@@ -17,36 +17,33 @@ public class Sprite : ISprite {
     public Color Tint { get; set; } = Color.WHITE;
 
     public void Draw(Vector2 pos, float rot, Vector2 origin, Vector2 size, Vector2 worldToScreenSpace){
-        // Raylib.DrawTexturePro(
-        //     Texture, 
-        //     SelectionBox, 
-        //     new Rectangle(pos.X, pos.Y, size.X, size.Y), // WTF why does this take pos & box, and not only box?
-        //     origin, // needed for rotation
-        //     rot, 
-        //     Tint
-        // );
-        //
-        Vector2 screenPos = pos * worldToScreenSpace;
-        Vector2 screenSize = size * worldToScreenSpace;
+        var scaledOrigin = origin * worldToScreenSpace;
+        var screenPos = pos * worldToScreenSpace;
         Raylib.DrawTexturePro(
-            Texture, 
-            SelectionBox, 
-            new Rectangle(screenPos.X, screenPos.Y, screenSize.X, screenSize.Y),
-            origin * worldToScreenSpace, // origin scaled as well
-            rot, 
+            Texture,
+            SelectionBox,
+            new Rectangle(screenPos.X, screenPos.Y, size.X * worldToScreenSpace.X, size.Y * worldToScreenSpace.Y),
+            scaledOrigin, // we use the scaledOrigin here.
+            rot,
             Tint
         );
     }
     
     public void DrawDebug(Vector2 pos, float rot, Vector2 origin, Vector2 size, Rectangle box, Vector2 worldToScreenSpace){
         // BOUNDING BOX
-        Rectangle screenBox = new Rectangle(box.X * worldToScreenSpace.X, box.Y * worldToScreenSpace.Y, size.X * worldToScreenSpace.X, size.Y * worldToScreenSpace.Y);
+        var screenPosX = pos.X * worldToScreenSpace.X;
+        var screenPosY = pos.Y * worldToScreenSpace.Y;
+        var screenSizeX = size.X * worldToScreenSpace.X;
+        var screenSizeY = size.Y * worldToScreenSpace.Y;
+
+        // BOUNDING BOX
+        var screenBox = new Rectangle(box.X * worldToScreenSpace.X, box.Y * worldToScreenSpace.Y, screenSizeX, screenSizeY);
         Raylib.DrawRectangleLines((int)screenBox.X, (int)screenBox.Y, (int)screenBox.Width, (int)screenBox.Height, Color.RED);
 
         // ROTATION
         const float length = 2000;
-        Vector2 screenPos = pos * worldToScreenSpace;
-        Vector2 endPoint = screenPos + new Vector2((float)Math.Cos(Raylib.DEG2RAD * rot), (float)Math.Sin(Raylib.DEG2RAD * rot)) * length; 
+        Vector2 screenPos = new Vector2(screenPosX, screenPosY);
+        Vector2 endPoint = screenPos + new Vector2((float)Math.Cos(Raylib.DEG2RAD * rot), (float)Math.Sin(Raylib.DEG2RAD * rot)) * length;
         Raylib.DrawLineV(screenPos, endPoint, Color.DARKGREEN);
     }
 }
