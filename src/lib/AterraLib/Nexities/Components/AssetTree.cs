e0a52aeaf5e01ade361829de39d8b3430ea9d876
AndreasSasDev <directive.athena@gmail.com>
2024-08-13 14:23:28 +0200

    Update AssetTree.cs

--- src/lib/AterraLib/Nexities/Components/AssetTree.cs
+++ src/lib/AterraLib/Nexities/Components/AssetTree.cs
@@ -12,20 +12,20 @@ namespace AterraLib.Nexities.Components;
 [Component<IAssetTree>("AterraLib:Nexities/Components/AssetTree")]
 [UsedImplicitly]
 public class AssetTree : NexitiesComponent, IAssetTree {
-    private readonly List<IAssetInstance> _nodes = [];
+    private readonly List<IAssetInstance> _directChildren = [];
     
     private int? _countCache;
     public int Count => _countCache ??= GetCount();
     
     private IAssetInstance[]? _cachedSnapshot;
     private IAssetInstance[] UpdateSnapshotCache() {
-        lock (_nodes) return _nodes.ToArray();
+        lock (_directChildren) return _directChildren.ToArray();
     }
     private IAssetInstance[] CachedSnapshot => _cachedSnapshot ??= UpdateSnapshotCache();
     
     private IAssetInstance[]? _cachedSnapshotReverse;
     private IAssetInstance[] UpdateSnapshotReverseCache() {
-        lock (_nodes) return _nodes.ToArray().Reverse().ToArray();
+        lock (_directChildren) return _directChildren.ToArray().Reverse().ToArray();
     }
     private IAssetInstance[] CachedSnapshotReverse => _cachedSnapshotReverse ??= UpdateSnapshotReverseCache();
 
@@ -34,8 +34,8 @@ public class AssetTree : NexitiesComponent, IAssetTree {
     // -----------------------------------------------------------------------------------------------------------------
     #region GetCount
     private int GetCount() {
-        lock (_nodes) {
-            return _nodes.Count;
+        lock (_directChildren) {
+            return _directChildren.Count;
         }
     }
     #endregion
@@ -84,8 +84,8 @@ public class AssetTree : NexitiesComponent, IAssetTree {
     // -----------------------------------------------------------------------------------------------------------------
     #region Add
     public void Add<T>(T node) where T : IAssetInstance {
-        lock (_nodes) {
-            _nodes.Add(node);
+        lock (_directChildren) {
+            _directChildren.Add(node);
         }
         lock (CachedSnapshot) {
             _cachedSnapshot = null;
@@ -95,8 +95,8 @@ public class AssetTree : NexitiesComponent, IAssetTree {
         }
     }
     public void AddFirst<T>(T node) where T : IAssetInstance {
-        lock (_nodes) {
-            _nodes.Insert(0, node);
+        lock (_directChildren) {
+            _directChildren.Insert(0, node);
         }
         lock (CachedSnapshot) {
             _cachedSnapshot = null;
