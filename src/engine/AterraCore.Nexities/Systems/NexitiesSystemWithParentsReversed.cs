714236a8ab7be8da7f08889a3549028f53bab045
AndreasSasDev <directive.athena@gmail.com>
2024-09-06 20:18:37 +0200

    Fix: Pooling of temp list

--- src/engine/AterraCore.Nexities/Systems/NexitiesSystemWithParentsReversed.cs
+++ src/engine/AterraCore.Nexities/Systems/NexitiesSystemWithParentsReversed.cs
@@ -4,6 +4,7 @@
 using AterraCore.Contracts.OmniVault.Assets;
 using AterraCore.Contracts.OmniVault.World;
 using JetBrains.Annotations;
+using Microsoft.Extensions.ObjectPool;
 
 namespace AterraCore.Nexities.Systems;
 // ---------------------------------------------------------------------------------------------------------------------
@@ -13,16 +14,23 @@ namespace AterraCore.Nexities.Systems;
 public abstract class NexitiesSystemWithParentsReversed<TParent, TChild> : NexitiesSystemWithParents<TParent, TChild>
     where TParent : class, IAssetInstance
     where TChild : class, IAssetInstance {
+    
+    // -----------------------------------------------------------------------------------------------------------------
+    // Methods
+    // -----------------------------------------------------------------------------------------------------------------
     protected override (TParent? Parent, TChild Child)[] GetEntities(ActiveLevel level) {
         if (BufferPopulated) return EntitiesBuffer;
 
-        var list = new List<(TParent? Parent, TChild Child)>();
+        List<(TParent? Parent, TChild Child)> list = ParentChildPool.Get();
         foreach ((IAssetInstance? Parent, IAssetInstance Child) instance in level.ActiveEntityTree.GetAsFlatReverseWithParent()) {
-            if (instance.Child is TChild child)
-                list.Add((instance.Parent as TParent, child));
+            if (instance.Child is TChild child) list.Add((instance.Parent as TParent, child));
         }
 
         BufferPopulated = true;
-        return EntitiesBuffer = list.ToArray();
+        list.TrimExcess();
+        EntitiesBuffer = list.ToArray();
+        
+        ParentChildPool.Return(list);
+        return EntitiesBuffer;
     }
 }
