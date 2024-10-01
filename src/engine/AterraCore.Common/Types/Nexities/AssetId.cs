951714f221dcb506f668f116fa0b929bc18b4479
AndreasSasDev <directive.athena@gmail.com>
2024-10-01 16:37:27 +0200

    Change: AssetId size limit

--- src/engine/AterraCore.Common/Types/Nexities/AssetId.cs
+++ src/engine/AterraCore.Common/Types/Nexities/AssetId.cs
@@ -3,7 +3,6 @@
 // ---------------------------------------------------------------------------------------------------------------------
 using System.Collections.Concurrent;
 using System.Numerics;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
@@ -15,13 +14,15 @@ namespace AterraCore.Common.Types.Nexities;
 public readonly struct AssetId :
     IEqualityOperators<AssetId, AssetId, bool>,
     IEquatable<AssetId>,
-    IEqualityOperators<AssetId, PluginId, bool> {
+    IEqualityOperators<AssetId, PluginId, bool> 
+{
+    private const int MaxLength = 255;
     public readonly PluginId PluginId;
     public readonly NameSpace NameSpace;
     private readonly int _hashCode;
     private readonly ReadOnlyMemory<char> _assetIdCache;
     private static readonly ConcurrentDictionary<string, (PluginId pluginId, NameSpace assetName, ReadOnlyMemory<char> cache)> GlobalCache = new();
-
+    
     // -----------------------------------------------------------------------------------------------------------------
     // Constructors
     // -----------------------------------------------------------------------------------------------------------------
@@ -34,9 +35,14 @@ public readonly struct AssetId :
         NameSpace = assetName;
         _hashCode = ComputeHashCode();
         _assetIdCache = GetOrAddCache(pluginId, assetName).cache;
+
+        if (_assetIdCache.Length <= MaxLength) return;
+        GlobalCache.TryRemove(_assetIdCache.ToString(), out _);
+        throw new ArgumentException("AssetId length cannot exceed 256 characters");
     }
 
     public AssetId(string assetId) {
+        if (assetId.Length > MaxLength) throw new ArgumentException("AssetId length cannot exceed 256 characters");
         (PluginId pluginId, NameSpace assetName, ReadOnlyMemory<char> cache) = GlobalCache.GetOrAdd(assetId, valueFactory: id => {
             (PluginId pluginId, NameSpace assetName) = ParseAssetId(id);
             return (pluginId, assetName, GetAsMemory(pluginId, assetName));
@@ -75,7 +81,10 @@ public readonly struct AssetId :
 
     private static (PluginId, NameSpace, ReadOnlyMemory<char> cache) GetOrAddCache(PluginId pluginId, NameSpace assetName) {
         string key = pluginId.Value + ':' + string.Join('/', assetName);
-        return GlobalCache.GetOrAdd(key, valueFactory: _ => (pluginId, assetName, GetAsMemory(pluginId, assetName)));
+        return GlobalCache.GetOrAdd(
+            key,
+            _ => (pluginId, assetName, GetAsMemory(pluginId, assetName))
+        );
     }
 
     private static ReadOnlyMemory<char> GetAsMemory(PluginId pluginId, NameSpace assetName) {
@@ -84,32 +93,24 @@ public readonly struct AssetId :
     }
 
     public override string ToString() => _assetIdCache.ToString();
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public override int GetHashCode() => _hashCode;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     private int ComputeHashCode() => HashCode.Combine(PluginId, NameSpace);
 
     // -----------------------------------------------------------------------------------------------------------------
     // Comparison Methods
     // -----------------------------------------------------------------------------------------------------------------
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
     public static bool operator ==(AssetId left, AssetId right) => left.Equals(right);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public static bool operator !=(AssetId left, AssetId right) => !left.Equals(right);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public static bool operator ==(AssetId left, PluginId right) => left.PluginId == right;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public static bool operator !=(AssetId left, PluginId right) => left.PluginId != right;
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public override bool Equals(object? obj) => obj is AssetId other && Equals(other);
-
-    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    
     public bool Equals(AssetId other) =>
         PluginId.Equals(other.PluginId)
         && NameSpace.Equals(other.NameSpace);
